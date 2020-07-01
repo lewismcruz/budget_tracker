b35d6cb539d936c00df312f5bbbd7d4d9292634c
lewismcruz <lewis.cruz@gmail.com>
2020-07-01 01:02:11 -0500

    Created db.js to create indexedDB instance and handle offline browser data storage.

--- server.js
+++ server.js
@@ -3,9 +3,11 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
+//const PORT = process.env.PORT || 3000;
 const PORT = 3000;
 
 const app = express();
+
 
 app.use(logger("dev"));
 
@@ -14,6 +16,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
+
+// Use this line if we use process.env above (process.env.MONGODB_URI || "mongodb://localhost/budget", {
 
 mongoose.connect("mongodb://localhost/budget", {
   useNewUrlParser: true,
